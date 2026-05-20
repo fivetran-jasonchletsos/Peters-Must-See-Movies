9671409901406a80ce57214b3188a41a85127f86
fivetran-jasonchletsos <jason.chletsos@fivetran.com>
2026-05-20 19:06:38 -0400

    Deploy: senior reskin + /add page

---
+++ _next/static/chunks/app/movie/[slug]/page-64228b6268b067fe.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[867,160,109,571,761],{4807:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=4807)}),_N_E=n.O()}]);
